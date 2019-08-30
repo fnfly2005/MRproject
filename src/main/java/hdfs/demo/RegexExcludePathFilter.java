ce01743277398679d64688b8c52148ba457bca9c
fnfly2005 <fnfly2005@aliyun.com>
2019-08-30 21:06:30 +0800

    学习hadoop-java接口文件系统pathFilter 过滤器对象

---
+++ src/main/java/hdfs/demo/RegexExcludePathFilter.java
@@ -0,0 +1,21 @@
+package hdfs.demo;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.PathFilter;
+
+public class RegexExcludePathFilter implements PathFilter {
+	/*
+	 * PathFilter用于排除匹配正则表达式的路径
+	 */
+	private final String regex;
+
+	public RegexExcludePathFilter(String regex) {
+		// TODO Auto-generated constructor stub
+		this.regex = regex;
+	}
+	@Override
+	public boolean accept(Path path) {
+		return !path.toString().matches(regex);
+	}
+
+}
--- src/main/java/hdfs/demo/ShowFileStatusTest.java
+++
@@ -1,9 +0,0 @@
-package hdfs.demo;
-
-public class ShowFileStatusTest {
-
-	public static void main(String[] args) {
-
-	}
-
-}
