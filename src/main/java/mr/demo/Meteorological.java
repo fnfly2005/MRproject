7c30f1a8b434c2b44ee8d4e8b8c5f0ece3a56648
fnfly2005 <fnfly2005@aliyun.com>
2019-09-16 21:30:48 +0800

    学习hadoop多个类使用maven打包

--- src/main/java/mr/demo/Meteorological.java
+++ src/main/java/mr/demo/Meteorological.java
@@ -1,15 +1,10 @@
 package mr.demo;
 
-import java.io.IOException;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
@@ -17,51 +12,11 @@ import org.apache.hadoop.util.GenericOptionsParser;
 
 public class Meteorological {
 	
-
-		private static class MaxTemperatureMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
-			private static final int Missing = 9999;
-
-			@Override
-			protected void map(LongWritable key, Text value,
-					Mapper<LongWritable, Text, Text, IntWritable>.Context context)
-					throws IOException, InterruptedException {
-				String line = value.toString();
-				String year = line.substring(15,19);
-				int airTemperature;
-				if(line.charAt(87) == '+') {
-					airTemperature = Integer.parseInt(line.substring(88,92));
-				} else {
-					airTemperature = Integer.parseInt(line.substring(87,92));
-				}
-				String quality = line.substring(92,93);
-				if (airTemperature !=Missing && quality.matches("[01459]")) {
-					context.write(new Text(year), new IntWritable(airTemperature));
-				}
-			}
-
-		}
-		
-		private static class MaxTemperatureReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
-
-			@Override
-			protected void reduce(Text key, Iterable<IntWritable> values,
-					Reducer<Text, IntWritable, Text, IntWritable>.Context context)
-					throws IOException, InterruptedException {
-				int maxValue = Integer.MIN_VALUE;
-				for (IntWritable value: values) {
-					maxValue= Math.max(maxValue,value.get());
-				}
-				context.write(key, new IntWritable(maxValue));
-			}
-
-		}		
-
-
 	public static void main(String[] args) throws Exception {
 		
 		if (args.length !=2)
 		{
-			System.err.println("本程序需要两个参数执行，hadoop jar Meteorological.jar /input/info/txt /output");
+			System.err.println("本程序需要两个参数执行，hadoop mr.demo.Meteorological /input/info/txt /output");
 			System.exit(1);
 		}
 		Configuration conf = new Configuration();
