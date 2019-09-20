ac81e8db9714edb53d404004c4e9683eeee76d7d
fnfly2005 <fnfly2005@aliyun.com>
2019-09-20 09:44:37 +0800

    Hadoop的I/O操作-序列化-Writable类-Text类型一

--- src/main/java/io/demo/SerializeDemo.java
+++ src/main/java/io/demo/SerializeDemo.java
@@ -1,5 +1,7 @@
 package io.demo;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
@@ -12,53 +14,73 @@ import org.apache.hadoop.io.VIntWritable;
 import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.io.WritableComparator;
 import org.apache.hadoop.util.StringUtils;
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
 
 public class SerializeDemo {
 
 	public static void main(String[] args) throws IOException {
-		System.out.println("Hadoop的I/O操作-序列化-Writable接口一");
-		IntWritable writable1 = new IntWritable();
-		writable1.set(163);
-		IntWritable writable2 = new IntWritable(164);
-		byte[] bytes = serialize(writable1);
 
-		System.out.println(writable1 + ":" + writable2 + ":" + bytes.length);
-		System.out.println("序列化："+StringUtils.byteToHexString(bytes));
-
-		IntWritable newWritable = new IntWritable();
-		deserialize(newWritable, bytes);
-		System.out.println("反序列化："+newWritable.get());
-
-		System.out.println("Hadoop的I/O操作-序列化-Writable接口二");
-		@SuppressWarnings("unchecked")
-		RawComparator<IntWritable> comparator = WritableComparator.get(IntWritable.class);
-
-		// 比较IntWritable对象
-		System.out.println("常规数据比较："+comparator.compare(writable1, writable2));// 相同为0，1>2为1，1<2为-1
-
-		// 比较序列化对象
-		byte[] b1 = serialize(writable1);
-		byte[] b2 = serialize(writable2);
-		System.out.println("原始字节数据比较："+comparator.compare(b1, 0, b1.length, b2, 0, b2.length));
-
-		System.out.println("Hadoop的I/O操作-序列化-Writable类");
-		// Java基本类型Writable封装器
-		byte[] data = serialize(new VIntWritable(163));
-		System.out.println(StringUtils.byteToHexString(data));
-		
-		System.out.println("Hadoop的I/O操作-序列化-Writable类-Text类型");
-		Text t = new Text("hadoop");
-		System.out.println(t.getLength());
-		System.out.println(t.getBytes().length);
-		System.out.println(t.charAt(2));
-		System.out.println(t.charAt(100));
-		System.out.println("find a substring:" + t.find("do"));
-		System.out.println("finds first o:" + t.find("o"));
-		System.out.println("finds o from position 4 or later:" + t.find("o",4));
-		System.out.println("No match:"+t.find("pig"));
+		Result result = JUnitCore.runClasses(SerializeDemo.class);
+		for (Failure failure :result.getFailures()) {
+			System.out.println(failure.toString());
+		}
+		System.out.println(result.wasSuccessful());
 
 	}
 
+	@Test
+	public void writableInterfaceDemo() throws IOException {
+		//Hadoop的I/O操作-序列化-Writable接口一
+		IntWritable writable = new IntWritable();
+		writable.set(163);
+		byte[] bytes = serialize(writable);
+		
+		assertEquals(bytes.length, 4);
+		assertEquals(StringUtils.byteToHexString(bytes), "000000a3");
+		
+		IntWritable newWritable = new IntWritable();
+		deserialize(newWritable, bytes);
+		assertEquals(newWritable.get(), 163);
+		
+		//Hadoop的I/O操作-序列化-Writable接口二
+		@SuppressWarnings("unchecked")
+		RawComparator<IntWritable> comparator = WritableComparator.get(IntWritable.class);
+
+		IntWritable w1 = new IntWritable(163);
+		IntWritable w2 = new IntWritable(67);
+		// 比较IntWritable对象
+		assertEquals(comparator.compare(w1, w2), 1);// 相同为0，1>2为1，1<2为-1
+
+		// 比较序列化对象
+		byte[] b1 = serialize(w1);
+		byte[] b2 = serialize(w2);
+		assertEquals(comparator.compare(b1, 0, b1.length, b2, 0, b2.length), 1);	
+	}
+	
+	@Test
+	public void writableClassDemo() throws IOException {
+		//Hadoop的I/O操作-序列化-Writable类
+		// Java基本类型Writable封装器
+		byte[] data = serialize(new VIntWritable(163));
+		assertEquals(StringUtils.byteToHexString(data), "8fa3");
+		
+		//Hadoop的I/O操作-序列化-Writable类-Text类型一
+		Text t = new Text("hadoop");
+		assertEquals(t.getLength(), 6);
+		assertEquals(t.getBytes().length,6);
+		assertEquals(t.charAt(2),(int)'d');
+		
+		assertEquals(t.charAt(100),-1);//Out of bounds
+		assertEquals(t.find("do"),2);//find a substring
+		assertEquals(t.find("o"),3);//finds first o
+		assertEquals(t.find("o",4),4);//finds o from position 4 or later
+		assertEquals(t.find("pig"),-1);//No match
+	}
+	
 	public static byte[] serialize(Writable writable) throws IOException {
 
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
