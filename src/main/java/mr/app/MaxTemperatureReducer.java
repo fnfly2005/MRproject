6dcc677d192277f43b147dd898e958a6b92a8bab
fnfly2005 <fnfly2005@aliyun.com>
2019-10-29 20:14:56 +0800

    MapReduce应用开发-用MRUnit来写单元测试-关于Reducer

--- src/main/java/mr/app/MaxTemperatureReducer.java
+++ src/main/java/mr/app/MaxTemperatureReducer.java
@@ -7,15 +7,21 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class MaxTemperatureReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+	/*
+	 * MapReduce应用开发-用MRUnit来写单元测试-关于Reducer
+	 * 范例6-9. 用来计算最高气温的reducer
+	 * hadoop mapreduce Reducer<KEYIN,VALUEIN,KEYOUT,VALUEOUT> 类
+	 */
 
 	@Override
 	protected void reduce(Text key, Iterable<IntWritable> values,
 			Context context) throws IOException, InterruptedException {
-		
-		int maxValue = Integer.MIN_VALUE;
+		//reduce(KEYIN key,Iterable<VALUEIN> values,Reducer.Context context)
+		//对每个键调用一次此方法。大多数应用程序将通过重写此方法来定义它们的reduce类
+		int maxValue = Integer.MIN_VALUE;//MIN_VALUE A constant holding the minimum value an int can have, -231
 		for (IntWritable value : values) {
-			maxValue = Math.max(maxValue, value.get());
-		}
+			maxValue = Math.max(maxValue, value.get());//Math.max(a,b) Returns the greater of two values
+		}//IntWritable.get() Return the value of this IntWritable.
 		context.write(key, new IntWritable(maxValue));
 	}
 	
