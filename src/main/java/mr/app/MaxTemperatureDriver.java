9f77d61230ced37adb720ce3dffd930909139e2e
fnfly2005 <fnfly2005@aliyun.com>
2019-10-30 20:47:24 +0800

    MapReduce应用开发-本地运行测试数据

--- src/main/java/mr/app/MaxTemperatureDriver.java
+++ src/main/java/mr/app/MaxTemperatureDriver.java
@@ -12,18 +12,27 @@ import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 public class MaxTemperatureDriver extends Configured implements Tool {
+	//Configured 用于可以使用Configuration 的基类 
+	//Tool 处理命令行的工具包
+	/*
+	 * MapReduce应用开发-本地运行测试数据
+	 * 范例6-10 查找最高气温
+	 */
 
 	@Override
 	public int run(String[] args) throws Exception {
-		
+		//Tool.run(String[] args) Execute the command with the given arguments.
 		if(args.length != 2) {
 			System.err.printf("Usage: %s [generic options] <input> <output>\n",
 					getClass().getSimpleName());
+			//getClass() Returns the runtime class of this Object
+			//getSimpleName() Returns the simple name of the underlying class as given in the source code
 			ToolRunner.printGenericCommandUsage(System.err);
+			//ToolRunner can be used to run classes implementing Tool interface
 			return -1;
 		}
 		
-		Configuration conf = new Configuration();
+		Configuration conf = new Configuration();//Configuration() Provides access to configuration parameters
 		Job job = Job.getInstance(conf,"Max temperature");
 		job.setJarByClass(getClass());
 		
